939bfbd31cd2df7aacb09e579a92ef0f0e128878
Кирилл Печенко <kirillcomcom5@gmail.com>
2020-09-12 09:09:52 +0300

    v26

--- Vkbot.ASP/Source/Commands/HelpCommandMessage.cs
+++ Vkbot.ASP/Source/Commands/HelpCommandMessage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VkNet.Abstractions;
 using VkNet.Model;
 
 namespace Vkbot.ASP.Source
@@ -10,7 +11,7 @@ namespace Vkbot.ASP.Source
     [BotCommand("/help")]
     public class HelpCommandMessage : BindingMessage
     {
-        public override string Do(Message message)
+        public override string Do(IVkApi vkApi,Message message)
         {
             return "Это тестовый бот написаный на C# ASP.NET CORE!";
         }
